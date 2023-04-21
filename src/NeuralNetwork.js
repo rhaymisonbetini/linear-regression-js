1004d6c65ea2fe9be53ca99f3e13f086c1ba430c
RHAYMISON  CRISTIAN <rhaymison@hotmail.com>
2023-04-21 18:03:59 -0300

    train-load-model-web-node

--- src/NeuralNetwork.js
+++ src/NeuralNetwork.js
@@ -1,28 +1,44 @@
 'use strict'
 const tf = require('@tensorflow/tfjs');
+const tfn = require('@tensorflow/tfjs-node')
 const Utils = require('./helpers/Utils');
 
 class NeuralNetwork {
 
-    async compileModel() {
-        const model = tf.sequential();
-        model.add(tf.layers.dense({ units: 1, inputShape: [1] }))
+    async compileModelNode(datas, corresponding, epochs) {
+        const model = tfn.sequential();
+        model.add(tfn.layers.dense({ units: 1, inputShape: [1] }))
         model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+        const x = tfn.tensor(datas, [datas.length, 1]);
+        const y = tfn.tensor(corresponding) //[10], [20], [30], [40]
+        await model.fit(x, y, {
+            epochs: epochs, callbacks: {
+                onBatchEnd: (bath, logs) => {
+                    console.log(logs.loss)
+                }
+            }
+        })
         return model;
     }
 
-
-    async linearRegressionNt() {
-        let predictInput = [5, 6, 7, 8, 9, 10];
-        const x = tf.tensor([1, 2, 3, 4], [4, 1]); //original values
-        const y = tf.tensor([[10], [20], [30], [40]]) // train
-        const input = tf.tensor(predictInput, [predictInput.length, 1]) // inptus to predict
-        let model = await this.compileModel();
+    async compileModel(datas, corresponding,epochs) {
+        const model = tf.sequential();
+        model.add(tf.layers.dense({ units: 1, inputShape: [1] }))
+        model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+        const x = tf.tensor(datas, [datas.length, 1]);
+        const y = tf.tensor(corresponding) //[10], [20], [30], [40]
         await model.fit(x, y, {
-            epochs: 2500, callbacks: { onBatchEnd:(bath, logs) =>{
-                console.log(logs.loss)
-            }}
+            epochs: epochs, callbacks: {
+                onBatchEnd: (bath, logs) => {
+                    console.log(logs.loss)
+                }
+            }
         })
+        return model;
+    }
+
+    async linearRegressionNt(model,predictInput) {
+        const input = tf.tensor(predictInput, [predictInput.length, 1]) // inptus to predict
         let output = model.predict(input).dataSync()
         output = Utils.convertTensorPredictToarray(output)
         return {
@@ -30,7 +46,7 @@ class NeuralNetwork {
             prefict: output
         }
     }
-   
+
 }
 
 module.exports = NeuralNetwork;
