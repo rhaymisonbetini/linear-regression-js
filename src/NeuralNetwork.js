daf389b35ce115831de17bb0786807aeeac0822d
RHAYMISON  CRISTIAN <rhaymison@hotmail.com>
2023-05-01 10:28:07 -0300

    finish-node-predict

--- src/NeuralNetwork.js
+++ src/NeuralNetwork.js
@@ -1,5 +1,4 @@
 'use strict'
-const tf = require('@tensorflow/tfjs');
 const tfn = require('@tensorflow/tfjs-node')
 const Utils = require('./helpers/Utils');
 
@@ -21,24 +20,8 @@ class NeuralNetwork {
         return model;
     }
 
-    async compileModel(datas, corresponding,epochs) {
-        const model = tf.sequential();
-        model.add(tf.layers.dense({ units: 1, inputShape: [1] }))
-        model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
-        const x = tf.tensor(datas, [datas.length, 1]);
-        const y = tf.tensor(corresponding) //[10], [20], [30], [40]
-        await model.fit(x, y, {
-            epochs: epochs, callbacks: {
-                onBatchEnd: (bath, logs) => {
-                    console.log(logs.loss)
-                }
-            }
-        })
-        return model;
-    }
-
     async linearRegressionNt(model,predictInput) {
-        const input = tf.tensor(predictInput, [predictInput.length, 1]) // inptus to predict
+        const input = tfn.tensor(predictInput, [predictInput.length, 1]) // inptus to predict
         let output = model.predict(input).dataSync()
         output = Utils.convertTensorPredictToarray(output)
         return {
